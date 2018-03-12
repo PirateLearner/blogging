26ad7ee682e9a2dfaf53bf4b11926154a83f67c7
anshulthakur <anshulthakur@rediffmail.com>
2018-03-12 22:04:32 +0530

    Basic CRUD tests for Content Model.

--- demo/blogging/models.py
+++ demo/blogging/models.py
@@ -4,7 +4,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class blogContent(models.Model):
+class Content(models.Model):
     """
     Model for a raw blog database entry.
     """
--- demo/blogging/tests/test_models.py
+++ demo/blogging/tests/test_models.py
@@ -8,6 +8,7 @@ from django.test import TestCase
 from blogging import models
 from django.contrib.auth.models import User
 
+
 class BaseTest(TestCase):
 
     def setUp(self):
@@ -21,27 +22,100 @@ class BaseTest(TestCase):
 
 
 class ContentModel(BaseTest):
-    def test_create_content(self):
-        obj = models.blogContent()
+    def test_create_content_fetch_by_id(self):
+        obj = models.Content()
         obj.author = self.user
         obj.title = "Test Post"
         obj.data = "We are entering some test data into this to test creation."
         
         obj.save()
         
-        from_db = models.blogContent.objects.get(title="Test Post")
+        from_db = models.Content.objects.get(id=obj.id)
         self.assertIsInstance(from_db, 
-                              models.blogContent, 
-                              "Fetched instance is not blogContent")
+                              models.Content, 
+                              "Fetched instance is not Content")
         self.assertEqual(from_db, obj, "Objects don't match")
         self.assertEqual(from_db.author, self.user, "Authors don't match")
         
+    def test_create_content_without_title_or_content(self):
+        obj = models.Content()
+        obj.author = self.user
+        
+        #Ideally, at least one of them should be specified.
+        # TODO
+        obj.save()
+        #If only content is specified, then use first 7 words as title.
+        
+    def test_create_content_fetch_by_title(self):
+        obj = models.Content()
+        obj.author = self.user
+        obj.title = "Test Post"
+        obj.data = "We are entering some test data into this to test creation."
+        
+        obj.save()
+        
+        from_db = models.Content.objects.get(title="Test Post")
+        self.assertIsInstance(from_db, 
+                              models.Content, 
+                              "Fetched instance is not Content")
+        self.assertEqual(from_db, obj, "Objects don't match")
+        self.assertEqual(from_db.author, self.user, "Authors don't match")
+        
+    def test_edit_content_title_field(self):
+        obj = models.Content.objects.create(author=self.user,
+                                                title="Test Post",
+                                                data = "We are entering some \
+                                                        test data into this to \
+                                                        test creation."
+                                                )
+        from_db = models.Content.objects.get(id=obj.id)
+        title = from_db.title
+        from_db.title = "Changed Title"
+        from_db.save()
+        
+        #Affirm we don't find it by previous title
+        from_db = models.Content.objects.get(id=obj.id)
+        self.assertNotEqual(title, from_db.title, "Titles must not be equal")
+        self.assertEqual("Changed Title", 
+                         from_db.title, 
+                         "Changed title must be equal")
+
+    def test_edit_content_data_field(self):
+        obj = models.Content.objects.create(author=self.user,
+                                                title="Test Post",
+                                                data = "We are entering some \
+                                                        test data into this to \
+                                                        test creation."
+                                                )
+        from_db = models.Content.objects.get(id=obj.id)
+        data = from_db.data
+        from_db.data = "We have slightly modified the data"
+        from_db.save()
+        
+        #Affirm we don't find it by previous title
+        from_db = models.Content.objects.get(id=obj.id)
+        self.assertNotEqual(data, 
+                            from_db.data, 
+                            "Data must not be equal to previous")
+        self.assertEqual("We have slightly modified the data", 
+                         from_db.data, 
+                         "Changed data must be equal")
     
-    def test_edit_content(self):
-        pass
     
     def test_delete_content(self):
-        pass
+        obj = models.Content.objects.create(author=self.user,
+                                                title="Test Post",
+                                                data = "We are entering some \
+                                                        test data into this to \
+                                                        test creation."
+                                                )
+        from_db = models.Content.objects.get(id=obj.id)
+        id = obj.id
+        from_db.delete()
+        
+        self.assertRaises(models.Content.DoesNotExist, 
+                          models.Content.objects.get, id=id)
+
     
     def test_create_multiple(self):
         pass
